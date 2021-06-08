b09d916c1081dd162c75517f5435c291453aaa48
iremarpag <73463510+iremarpag@users.noreply.github.com>
2021-06-08 14:22:54 +0300

    uploaded

---
+++ MVCSanalMarketProject/Scripts/ShoppingCart.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $("ul#AcilirMenu li.acilir1").hover(function () {
+        document.getElementById("Panel1").style.display = 'none';
+        if (document.getElementById("Panel1").style.display != 'none') {
+            $("div#Panel1").fadeOut("slow");
+        }
+        else {
+            $("div#Panel1").fadeIn("slow");
+        }
+    }, function () {
+        $("div#Panel1").fadeOut("slow");
+        document.getElementById("Panel1").style.display = 'none';
+    });
+});
