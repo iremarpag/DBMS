2563122578f6a0fae9315314c0479278dd0f121e
iremarpag <73463510+iremarpag@users.noreply.github.com>
2021-06-08 14:21:28 +0300

    uploaded

---
+++ MVCSanalMarketProject/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MVCSanalMarketProject
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jQuery.js",
+                        "~/Scripts/ShoppingCart.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/styles/bootstrap").Include(
+                    "~/Content/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/template/Style.css",
+                      "~/Content/template/ShoppingCart.css"));
+        }
+    }
+}
